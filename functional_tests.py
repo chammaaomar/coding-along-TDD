86eb0fbfa7361732619711b19523e8cd117c70d8
Omar Chammaa <chammaa.omar@gmail.com>
2020-03-29 12:58:19 -0400

    user can input multiple items and they're saved with auti-incr counter; redirect after post

--- functional_tests.py
+++ functional_tests.py
@@ -16,12 +16,12 @@ class NewVisitorTest(unittest.TestCase):
         input_box = self.browser.find_element_by_id('id_new_item')
         input_box.send_keys(item_text)
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(3)
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+        self.assertIn(row_text, [row.text.strip() for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # name has to start with test to run by test runner
