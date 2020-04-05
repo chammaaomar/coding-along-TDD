52618abdc037eb3a81313f563cba1b60ed688b59
Omar Chammaa <chammaa.omar@gmail.com>
2020-04-05 16:39:46 -0400

    added List model, and foregin key reference from item to parent list; still trivial

--- lists/models.py
+++ lists/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 # Create your models here.
-
 # real Django model inherits from Model
+
+
+class List(models.Model):
+    pass
 
 
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None, null=True)
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from lists.models import Item
+from lists.models import Item, List
 # Create your views here.
 
 
@@ -17,6 +17,7 @@ def view_list(request):
 
 def new_list(request):
     if request.method == 'POST':
+        list_ = List.objects.create()
         new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
+        Item.objects.create(text=new_item_text, list=list_)
     return redirect('/lists/unique-list/')
